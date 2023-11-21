6e6503fc9ba17c0425e592d66fab02f9b4de94ab
bqle <benjaminle2002@gmail.com>
2023-11-21 18:11:19 -0500

    AutState

--- src/NFATest.hs
+++ src/NFATest.hs
@@ -16,7 +16,7 @@ tAlphabetNFA =
   ]
 
 kleeneNFA :: NFA
-kleeneNFA = kleene alphabetNFA
+kleeneNFA = kleene alphabetNFA{uuid=1}
 
 tKleeneNFA = 
   "kleeneNFA" ~: TestList [
@@ -48,5 +48,7 @@ tAlternateNFA =
     accept alternateNFA "" ~?= False
   ]
 
+test_all :: IO Counts
+test_all = runTestTT $ TestList [tAlternateNFA, tAlphabetNFA, tAppendNFA, tKleeneNFA]
 
 
