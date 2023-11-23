ca0579527463618d1684bcf6ef3993bec1fb2643
Brandon <me@brandonwisnicki.com>
2023-11-23 16:04:57 -0500

    finished most of regexparser

--- src/NFA.hs
+++ src/NFA.hs
@@ -1,175 +1,217 @@
-module NFA where 
+module NFA where
 
-import qualified Data.List as List
-import Data.Set.Monad (Set, fromList
-  , empty) -- Note, we are using Data.Set.Monad instead of regular Set
-import Data.List (nub)
-import Test.HUnit (Assertion, Counts, Test (..), assert, runTestTT, (~:), (~?=))
-import qualified Test.QuickCheck as QC
-import Data.Foldable          (foldlM)
-import Control.Monad.Identity (Identity(..))
-import Data.Maybe (fromMaybe)
-import Data.Map (Map, fromList, lookup, empty, foldrWithKey, insert, union)
-import RandomString (randomUUID, hashString)
+-- Note, we are using Data.Set.Monad instead of regular Set
+
+import Control.Monad.Identity (Identity (..))
 import Crypto.Hash (hashWith)
+import Data.Foldable (foldlM)
+import Data.List (nub)
+import Data.List qualified as List
+import Data.Map (Map, empty, foldrWithKey, fromList, insert, lookup, union)
+import Data.Maybe (fromMaybe)
+import Data.Set.Monad
+  ( Set,
+    empty,
+    fromList,
+  )
+import RandomString (hashString, randomUUID)
+import Test.HUnit (Assertion, Counts, Test (..), assert, runTestTT, (~:), (~?=))
+import Test.QuickCheck qualified as QC
 
 -- { GENERAL AUTOMATON TYPES }
 
-data Automaton transitionT acceptingT = Aut {
-  uuid :: Int,
-  initial :: String,
-  transition :: transitionT,
-  accepting :: acceptingT 
-}
+data Automaton transitionT acceptingT = Aut
+  { uuid :: Int,
+    initial :: String,
+    transition :: transitionT,
+    accepting :: acceptingT
+  }
 
 -- | (current state, new char) -> new states
 type Transition v = Map (String, Char) v
 
 makeTransition :: Transition v -> v -> Char -> String -> v
-makeTransition trans def char st = 
+makeTransition trans def char st =
   fromMaybe def (Data.Map.lookup (st, char) trans)
 
-
 -- { NFA SPECIALIZATIONS }
-type NFATransition = Transition [String] 
+type NFATransition = Transition [String]
 
 -- | Finite NFA with state `s`, alphabet `a` and a monadic context `m`.
 --   The type parameters `s` and `a` are assumed to represent finite set
 --  Assumptions: 1 initial state, 1 accepting state
 type NFA = Automaton NFATransition String
 
-instance (Show transitionT, Show acceptingT) 
-  => Show (Automaton transitionT acceptingT) where
-  show Aut {uuid, initial, transition, accepting} 
-    = "{  uuid: " ++ show uuid ++ " ,\n" ++ 
-      "   initial" ++ show initial ++ " ,\n" ++ 
-      "   transitions: " ++ show transition ++ " ,\n" ++
-      "   accepting:" ++ show accepting ++ "\n }"
+instance
+  (Show transitionT, Show acceptingT) =>
+  Show (Automaton transitionT acceptingT)
+  where
+  show Aut {uuid, initial, transition, accepting} =
+    "{  uuid: "
+      ++ show uuid
+      ++ " ,\n"
+      ++ "   initial"
+      ++ show initial
+      ++ " ,\n"
+      ++ "   transitions: "
+      ++ show transition
+      ++ " ,\n"
+      ++ "   accepting:"
+      ++ show accepting
+      ++ "\n }"
 
 -- | Represents the epsilon transitions in NFAs
 epsilon = '\0'
 
-exampleNFA :: NFA 
-exampleNFA = Aut {
-  uuid = 0,
-  initial = "0", 
-  transition = Data.Map.fromList [(("0", 'a'), ["0"])],
-  accepting = "0"
-}
+exampleNFA :: NFA
+exampleNFA =
+  Aut
+    { uuid = 0,
+      initial = "0",
+      transition = Data.Map.fromList [(("0", 'a'), ["0"])],
+      accepting = "0"
+    }
 
 -- | Find all transitions after taking the char
 findNextStates :: NFATransition -> [String] -> Char -> [String]
-findNextStates transition starting a = 
-      nub $ exploreEpsilons transition 
-        (nub $ concatMap (makeTransition transition [] a ) starting)
+findNextStates transition starting a =
+  nub $
+    exploreEpsilons
+      transition
+      (nub $ concatMap (makeTransition transition [] a) starting)
 
 -- | Expand the states to include all reachable states thru epsilon transitions
 exploreEpsilons :: NFATransition -> [String] -> [String]
-exploreEpsilons transition states = 
-  let
-    immediateFrontier = nub $ concatMap 
-      (makeTransition transition [] epsilon) states
-  in 
-    case immediateFrontier of
-      [] -> states
-      _ -> exploreEpsilons transition immediateFrontier ++ states
+exploreEpsilons transition states =
+  let immediateFrontier =
+        nub $
+          concatMap
+            (makeTransition transition [] epsilon)
+            states
+   in case immediateFrontier of
+        [] -> states
+        _ -> exploreEpsilons transition immediateFrontier ++ states
 
 -- | Run an NFA & get the final states
-run ::  NFA -> [Char] -> [String]
-run Aut {initial, transition, accepting} = 
+run :: NFA -> [Char] -> [String]
+run Aut {initial, transition, accepting} =
   foldl (findNextStates transition) (exploreEpsilons transition [initial])
 
 accept :: NFA -> [Char] -> Bool
-accept nfa@Aut {accepting} s = 
-  (accepting `elem`) $ run nfa s 
+accept nfa@Aut {accepting} s =
+  (accepting `elem`) $ run nfa s
 
 -- | Attach the uuid to every state, update transitions accordingly
 attachUUID :: NFA -> NFA
 attachUUID Aut {uuid, initial, transition, accepting} =
   Aut uuid newInitial newTransitions newAccepting
-  where 
+  where
     hashWithUUID str = hashString (str ++ show uuid)
     newInitial = hashWithUUID initial
     newAccepting = hashWithUUID accepting
-    newTransitions = foldrWithKey (\(state, char) val
-      -> Data.Map.insert (hashWithUUID state, char) 
-        (List.map hashWithUUID val)) Data.Map.empty transition
+    newTransitions =
+      foldrWithKey
+        ( \(state, char) val ->
+            Data.Map.insert
+              (hashWithUUID state, char)
+              (List.map hashWithUUID val)
+        )
+        Data.Map.empty
+        transition
 
 -- | Union two transitions
 unionTransitions :: NFATransition -> NFATransition -> NFATransition
-unionTransitions t1 t2 = 
-  foldrWithKey (\(state, char) dest acc -> 
-    Data.Map.insert (state, char) (
-      nub (fromMaybe [] (Data.Map.lookup (state, char) acc) ++ dest)
-    ) acc
-  ) t2 t1
+unionTransitions t1 t2 =
+  foldrWithKey
+    ( \(state, char) dest acc ->
+        Data.Map.insert
+          (state, char)
+          ( nub (fromMaybe [] (Data.Map.lookup (state, char) acc) ++ dest)
+          )
+          acc
+    )
+    t2
+    t1
 
 -- | Create fully bipartite graph from two lists of vertices
---   We need original transitions because accepting states may already have 
+--   We need original transitions because accepting states may already have
 --   e-transitions
 bipartiteTransitions :: NFATransition -> [String] -> [String] -> NFATransition
-bipartiteTransitions transOrig s1 s2 = 
-  foldl (\acc acceptS -> 
-    foldl (\acc initS -> 
-      Data.Map.insert (acceptS, epsilon) (
-        nub (initS :
-          fromMaybe [] (Data.Map.lookup (acceptS, epsilon) acc) 
+bipartiteTransitions transOrig s1 s2 =
+  foldl
+    ( \acc acceptS ->
+        foldl
+          ( \acc initS ->
+              Data.Map.insert
+                (acceptS, epsilon)
+                ( nub
+                    ( initS
+                        : fromMaybe [] (Data.Map.lookup (acceptS, epsilon) acc)
+                    )
+                )
+                acc
           )
-      ) acc 
-    ) acc s2 
-  ) Data.Map.empty s1
+          acc
+          s2
+    )
+    Data.Map.empty
+    s1
 
 -- { CORE NFA Operations }
 
--- | Unit NFA accepts a set of chars 
+-- | Unit NFA accepts a set of chars
 alphabet :: [Char] -> NFA
-alphabet ls = 
-  let
-    initSt = "i"
-    accSt = "e"
-    transitions = foldl 
-      (\acc v -> Data.Map.insert (initSt, v) [accSt] acc) Data.Map.empty ls
-    in 
-    Aut 0 initSt transitions accSt
+alphabet ls =
+  let initSt = "i"
+      accSt = "e"
+      transitions =
+        foldl
+          (\acc v -> Data.Map.insert (initSt, v) [accSt] acc)
+          Data.Map.empty
+          ls
+   in Aut 0 initSt transitions accSt
 
 -- | Append 2 NFA (ab)
 append :: NFA -> NFA -> NFA
-append nfa1 nfa2 = 
+append nfa1 nfa2 =
   Aut 0 init1 newTransitions accept2
-  where 
-    Aut uuid1 init1 trans1 accept1 = attachUUID nfa1 
+  where
+    Aut uuid1 init1 trans1 accept1 = attachUUID nfa1
     Aut uuid2 init2 trans2 accept2 = attachUUID nfa2
     newConnections = bipartiteTransitions trans1 [accept1] [init2]
-    newTransitions = 
+    newTransitions =
       unionTransitions newConnections (unionTransitions trans1 trans2)
 
 -- | Alternate 2 NFA (a + b) (basically OR)
 alternate :: NFA -> NFA -> NFA
-alternate nfa1 nfa2 =  
-    let 
-      newInitSt = "i"
+alternate nfa1 nfa2 =
+  let newInitSt = "i"
       newAccSt = "e"
-      newConnections = unionTransitions
-        (bipartiteTransitions Data.Map.empty [newInitSt] [init1, init2])
-        (bipartiteTransitions 
-          (Data.Map.union trans1 trans2) [accept1 , accept2] [newAccSt])
-      newTransitions = unionTransitions newConnections 
-        (unionTransitions trans1 trans2) in
-
-      Aut 0 newInitSt newTransitions newAccSt 
-    where 
-      Aut uuid1 init1 trans1 accept1 = attachUUID nfa1 
-      Aut uuid2 init2 trans2 accept2 = attachUUID nfa2 
+      newConnections =
+        unionTransitions
+          (bipartiteTransitions Data.Map.empty [newInitSt] [init1, init2])
+          ( bipartiteTransitions
+              (Data.Map.union trans1 trans2)
+              [accept1, accept2]
+              [newAccSt]
+          )
+      newTransitions =
+        unionTransitions
+          newConnections
+          (unionTransitions trans1 trans2)
+   in Aut 0 newInitSt newTransitions newAccSt
+  where
+    Aut uuid1 init1 trans1 accept1 = attachUUID nfa1
+    Aut uuid2 init2 trans2 accept2 = attachUUID nfa2
 
 -- | kleene-star (a*)
 kleene :: NFA -> NFA
-kleene nfa = 
-  let 
-    newInitSt = "i"
-    newAccSt = "e"
-    Aut _ init trans accept = attachUUID nfa
-    newTransitions = 
-      unionTransitions 
-        (bipartiteTransitions Data.Map.empty [newInitSt] [newAccSt, init])
-        (bipartiteTransitions trans [accept] [newAccSt, init]) in
-  Aut 0 newInitSt (unionTransitions newTransitions trans) newAccSt
+kleene nfa =
+  let newInitSt = "i"
+      newAccSt = "e"
+      Aut _ init trans accept = attachUUID nfa
+      newTransitions =
+        unionTransitions
+          (bipartiteTransitions Data.Map.empty [newInitSt] [newAccSt, init])
+          (bipartiteTransitions trans [accept] [newAccSt, init])
+   in Aut 0 newInitSt (unionTransitions newTransitions trans) newAccSt
--- src/RegexParser.hs
+++ src/RegexParser.hs
@@ -2,8 +2,7 @@ module RegexParser () where
 
 import Data.Bifunctor
 import Data.Char (isAlpha)
-
--- import NFA (NFA)
+import NFA (NFA, accept, alphabet, alternate, append, kleene)
 
 frst :: (a, b, c) -> a
 frst (a, _, _) = a
@@ -27,7 +26,11 @@ peekStack :: String -> Char
 peekStack (x : xs) = x
 peekStack [] = 'ε'
 
-popStackUntil :: String -> (Char -> Bool) -> Bool -> (String, String)
+popStackUntil ::
+  String ->
+  (Char -> Bool) ->
+  Bool ->
+  (String, String)
 popStackUntil stack predicate removeFlaggedElem =
   let value =
         foldl
@@ -35,12 +38,30 @@ popStackUntil stack predicate removeFlaggedElem =
               if predicate x && thrd acc
                 then
                   if removeFlaggedElem
-                    then (frst acc, scnd acc, False)
-                    else (frst acc, scnd acc ++ [x], False)
+                    then
+                      ( frst acc,
+                        scnd acc,
+                        False
+                      )
+                    else
+                      ( frst acc,
+                        scnd acc ++ [x],
+                        False
+                      )
                 else
                   if thrd acc
-                    then (frst acc ++ [x], scnd acc, True)
-                    else (frst acc, scnd acc ++ [x], False)
+                    then
+                      ( frst acc ++ [x],
+                        scnd acc,
+                        True
+                      )
+                    else
+                      ( frst acc,
+                        scnd
+                          acc
+                          ++ [x],
+                        False
+                      )
           )
           ("", "", True)
           stack
@@ -56,7 +77,10 @@ injectConcatSymbol =
     ( \x acc -> case acc of
         [] -> [x]
         (hd : tl) ->
-          if not (elem x afterNoConcat || elem hd beforeNoConcat)
+          if not
+            ( elem x afterNoConcat
+                || elem hd beforeNoConcat
+            )
             then x : '@' : hd : tl
             else x : hd : tl
     )
@@ -64,17 +88,31 @@ injectConcatSymbol =
 
 regexToRPN :: String -> String
 regexToRPN regex =
-  let parsed = foldl parseCharacter ([], []) (injectConcatSymbol regex)
+  let parsed =
+        foldl
+          parseCharacter
+          ([], [])
+          (injectConcatSymbol regex)
    in uncurry (++) parsed
   where
     parseCharacter :: (String, String) -> Char -> (String, String)
     parseCharacter (outputQueue, operatorStack) c
-      | c `elem` operators && peekStack operatorStack == '(' = (outputQueue, c : operatorStack) -- done
+      | c `elem` operators
+          && peekStack operatorStack == '(' =
+          (outputQueue, c : operatorStack) -- done
       | c `elem` operators =
-          let updatedStack = popStackUntil operatorStack (\op -> precedence c < precedence op) False
+          let updatedStack =
+                popStackUntil
+                  operatorStack
+                  (\op -> precedence c < precedence op)
+                  False
            in bimap (outputQueue ++) (c :) updatedStack
       | c == ')' =
-          let updatedStack = popStackUntil operatorStack (== '(') True
+          let updatedStack =
+                popStackUntil
+                  operatorStack
+                  (== '(')
+                  True
            in first (outputQueue ++) updatedStack
       | c == '(' = (outputQueue, c : operatorStack) -- done
       | otherwise = (outputQueue ++ [c], operatorStack) -- done
@@ -84,5 +122,46 @@ regexToRPN regex =
 -- >>> injectConcatSymbol "a(a|b)*b"
 -- "a@(a|b)*@b"
 
--- rpnToNFA :: string -> NFA
--- rpnToNFA = undefined
+takeForBinary :: [a] -> Maybe (a, a, [a])
+takeForBinary nfas = case nfas of
+  x : y : arr -> Just (x, y, arr)
+  _ -> Nothing
+
+takeForUnary :: [a] -> Maybe (a, [a])
+takeForUnary nfas = case nfas of
+  x : arr -> Just (x, arr)
+  _ -> Nothing
+
+unwrapSingleElem :: Maybe [a] -> Maybe a
+unwrapSingleElem (Just [x]) = Just x
+unwrapSingleElem _ = Nothing
+
+rpnToNFA :: String -> Maybe NFA
+rpnToNFA rpn = unwrapSingleElem (foldl parseCharacter (Just []) rpn)
+  where
+    parseCharacter :: Maybe [NFA] -> Char -> Maybe [NFA]
+    parseCharacter Nothing _ = Nothing
+    parseCharacter (Just stack) c
+      | c `elem` operators = case c of
+          '*' ->
+            takeForUnary stack
+              >>= (\x -> Just (kleene (fst x) : snd x))
+          '|' ->
+            takeForBinary stack
+              >>= (\x -> Just (alternate (frst x) (scnd x) : thrd x))
+          '@' ->
+            takeForBinary stack
+              >>= (\x -> Just (append (frst x) (scnd x) : thrd x))
+          _ -> Nothing
+      | otherwise = Just (alphabet [c] : stack)
+
+regexToNFA :: String -> Maybe NFA
+regexToNFA regex = rpnToNFA (regexToRPN regex)
+
+-- "a(a|b)*b"
+
+test :: Maybe Bool
+test = regexToNFA "a*b" >>= \x -> Just (NFA.accept x "a")
+
+-- >>>  test
+-- ProgressCancelledException
