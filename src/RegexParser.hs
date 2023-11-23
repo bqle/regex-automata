5e41123e439099764f9d951c409721fc2fdfb3ff
Brandon <me@brandonwisnicki.com>
2023-11-23 02:34:04 -0500

    modified:   src/RegexParser.hs

--- src/RegexParser.hs
+++ src/RegexParser.hs
@@ -1,4 +1,29 @@
 module RegexParser () where
 
-test :: string -> string
-test x = x
+import Data.Char (isAlpha)
+
+-- import NFA (NFA)
+
+beforeNoConcat = "*|)"
+
+afterNoConcat = "(|"
+
+regexToRPN :: String -> String
+regexToRPN regex = undefined
+
+-- >>> injectConcatSymbol "abc(de)"
+-- "a_b_c_(d_e)"
+injectConcatSymbol :: String -> String
+injectConcatSymbol =
+  foldr
+    ( \x acc -> case acc of
+        [] -> [x]
+        (hd : tl) ->
+          if not (elem x afterNoConcat || elem hd beforeNoConcat)
+            then x : '@' : hd : tl
+            else x : hd : tl
+    )
+    []
+
+-- rpnToNFA :: string -> NFA
+-- rpnToNFA = undefined
